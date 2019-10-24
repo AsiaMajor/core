2415844ff6d1a2c641e9378b86bae2bc4281c40f
logic <lampoon2gn@gmail.com>
2019-10-24 16:11:45 -0700

    inittial

---
+++ tests/Preprocess Module.py
@@ -0,0 +1,17 @@
+import pytest
+from modules.search import Search
+
+def test_init_globals():
+    '''test globals in search module'''
+    pass
+    
+def test_retrive_wheel_data():
+    ''' test if the wheel data is correctly read in from db'''
+    pass
+
+# def test_retrive_column_data():
+#     pass
+
+def test_preprrocessing_new_sheet():
+    ''' test if the input column is properly proprocessed'''
+    pass
