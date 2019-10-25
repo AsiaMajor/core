4876d38d3e2589097c4e86ed3b006801d32f8fd6
YukaLangbuana <langbuana.yuka@hotmail.com>
2019-10-24 18:46:56 -0700

    core test

---
+++ tests/test_core.py
@@ -0,0 +1,14 @@
+import pytest
+from modules.ping import Ping
+
+def test_core_init():
+    pass
+
+def test_core_dbconn():
+    pass
+
+def test_core_apiresponse():
+    pass
+
+def test_core_fileinput():
+    pass
