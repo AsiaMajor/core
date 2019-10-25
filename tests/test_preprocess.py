31eaed9c7e59116ce5e30466e10f63a290081888
logic <lampoon2gn@gmail.com>
2019-10-24 19:08:56 -0700

    yo

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def test_init_globals_avg():
     '''test global vaerages generated for fingerprinting'''
+    pass
     sheet = [0,1,2,3,4,5,6,7,8,9]
     l,avg,std = PreProcess.get_wh_avg_std_data(sheet)
     
@@ -18,15 +19,23 @@ def test_init_globals_avg():
     assert std[0] is float
 
     
-def test_retrive_wheel_data(wheel_data):
+def test_retrive_wheel_data():
     ''' test if the wheel data is correctly read in from db'''
-    #assert wheel_data is 
     pass
+    col_num = 0
+    df = pd.DataFrame([[1,2,3,4,5],[6,7,8,9,10]])
+    out = PreProcess.get_n_column(df,col_num)
+
+    assert out is list
+    assert out[0] is float
 
 
 # def test_retrive_column_data():
 #     pass
 
 def test_preprrocessing_new_sheet():
-    ''' test if the input column is properly proprocessed'''
+    ''' test if the thresh is properly preprocessed'''
     pass
+    data = pd.DataFrame(range(1,101))
+    3_percent,50_percent,97_percent = ()
+    
