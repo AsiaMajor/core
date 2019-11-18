dc6589fc5a43a64425b25709a2ea74481ebe43f7
Bumjin Park <bjpark4002@gmail.com>
2019-11-17 17:36:43 -0800

    test_done
    -

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,7 @@
 import pytest
 from modules import search
 
-   
+    
 
 """
 commnad : python3 -m pytest test_search.py 
@@ -14,7 +14,16 @@ sheet4= "11111100"
 
 sheet5 = "11111111111111111111"
 sheet6 = "01010101010101010101"
-testClass = search.Controller(1,"sheet1","1111100000111110")
+
+sheet5_train_b_csv = "1001111000011000"
+sheet6_train_b_csv = "1100111000011100"
+
+# realTestData_Sheet6_train_b.csv = "1100111000011100"
+realClass = search.Controller(234,"input6","1100111000011100")
+
+
+
+testClass = search.Controller(221,"sheet1","1111100000111110")
                             # hashKey, sheetName, fingerPrint
                             # int,  string, string
 
@@ -35,5 +44,33 @@ def test_hamming_return_value():        # test when 2 inputs have 10 different f
     testV = testClass.hamming(sheet5,sheet6) # expected output is 10.
     assert testV == 10
 
-def test_database_connection_none_null():
+def test_database_connection_none_null():   # checks if the databse is conncected
     assert testClass.conn != None
+    
+
+def test_result_none_null():    # checks if the get_result() returns result
+    testV = testClass.get_result()
+    assert testV != None
+
+
+def test_result_bucket_type():  # check the return type == dict
+    testV = testClass.get_result() 
+    assert type(testV) == dict
+
+
+def test_result_sheet_none_null(): # check if it returns matching sheet name
+    testV = testClass.get_result()
+    assert testV["predicted"] != None
+
+
+def test_result_candidates_none_null(): #check if it also gives candidates of matching sheets.
+    testV = testClass.get_result()
+    assert testV["top_five"] != []
+
+def test_name_of_result_with_real_data(): # test real data with database.
+    realV = realClass.get_result()
+    assert realV["predicted"] == "sheet6_train_b.csv" 
+
+def test_candidates_of_result_with_real_data():
+    realV = realClass.get_result()
+    assert len(realV["top_five"]) >= 1
