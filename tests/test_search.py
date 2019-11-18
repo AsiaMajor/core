b71f50e4c165f11e2caf578d567aa0df916a5eb9
Bumjin Park <bjpark4002@gmail.com>
2019-11-17 17:40:30 -0800

    1

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,8 +1,23 @@
 import pytest
 from modules import search
+import redis
 
     
 
+f = open("final.csv").readlines()
+
+data = []
+for line in f:
+      data.append(line.replace('\n', '').split(','))
+conn = redis.StrictRedis(host="localhost", port=6379, db=2)
+for sheet in data[1:]:
+     res = conn.hset(sheet[0], sheet[1], sheet[2])
+     if res == 0:
+             print(sheet, "NOT INSERTED")
+     else:
+             print(sheet, "OK")
+
+             
 """
 commnad : python3 -m pytest test_search.py 
 """
