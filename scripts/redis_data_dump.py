aad95c92f0536a1dfb6e4e71c007806615da98d1
YukaLangbuana <langbuana.yuka@hotmail.com>
2019-12-04 15:32:55 -0800

    clean code

--- scripts/redis_data_dump.py
+++ scripts/redis_data_dump.py
@@ -1,15 +1,13 @@
 import redis
 
-# on github
-#f = open("redis/final.csv").readlines()
+f = open("/csv/fingerprints.csv").readlines()
 
-
-# on local
-f = open("../scripts/final.csv").readlines()
 data = []
 for line in f:
       data.append(line.replace('\n', '').split(','))
+
 conn = redis.StrictRedis(host="localhost", port=6379, db=2)
+
 for sheet in data[1:]:
      res = conn.hset(sheet[0], sheet[1], sheet[2])
      if res == 0:
@@ -19,4 +17,5 @@ for sheet in data[1:]:
 
 
 # you can check all contents in the buckets 
-# by running command hgetall [hashValue]
+# by running command HGETALL [hashValue]
+# and to reset: FLUSHALL
